4bb6b41cdf22ed387989b1a6f9046f37031bcb8f
uu9 <1161012497@qq.com>
2025-08-30 23:22:18 +0800

    Add: DirectML example code

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-import tensorflow as tf
+from datetime import datetime
+import torch
+import torch_directml
+from utils.log import logger
+
+DEVICE = torch_directml.device()
+logger.info(f"DirectML Info: {DEVICE}")
+
 
 def main():
-    print("Hello from fight-ai!")
-    print(tf.reduce_sum(tf.random.normal([1000, 1000])))
+    time_start = datetime.now()
+    size = 10000
+    tensor1 = torch.rand(size, size, device=DEVICE)
+    tensor2 = torch.rand(size, size, device=DEVICE)
+    dml_algebra = tensor1 * tensor2
+    time_end = datetime.now()
+    time_elapsed = time_end - time_start
+    print("time cost:")
+    print(time_elapsed.seconds)
 
 
 if __name__ == "__main__":
